d7de5d600021ef96d1571c23d9e6d7cf8fad7aa0
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-05-14 01:59:16 -0700

    improve code syntax

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -15,11 +15,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	if (new_node == NULL)
 		return (NULL);
 
-	if (parent == NULL)
-		new_node->parent = NULL;
-	else
-		new_node->parent = parent;
-
+	new_node->parent = parent;
 	new_node->n = value;
 	new_node->left = NULL;
 	new_node->right = NULL;

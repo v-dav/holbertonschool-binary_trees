fc5f7b3935fb32543f0ffb65467e02a3648c89eb
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-05-17 06:20:12 -0700

    correct the function

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -88,20 +88,20 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	size_t height = 0;
 	size_t left_height = 0;
 	size_t right_height = 0;
-	
+
 	if (!tree)
 		return (0);
 
 	if (!binary_tree_is_full(tree))
 		return (0);
-	
+
 	height = binary_tree_height(tree);
 
 	if (tree->left)
 		left_height = binary_tree_height(tree->left);
 	if (tree->right)
 		right_height = binary_tree_height(tree->right);
-	
+
 	if ((height == left_height) && (height == right_height))
 		return (1);
 	else
